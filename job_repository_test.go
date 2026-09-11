bd45c723c7f3dd3c361871f5ba2b976fdd7d6049
Kaisel Alcantara <Kaise@MacBook-Air.local>
2026-09-11 12:15:11 -0700

    Improve test cases

--- job_repository_test.go
+++ job_repository_test.go
@@ -2,43 +2,45 @@ package main
 
 import "testing"
 
-func Save_Test_Driven_Table(t *testing.T) {
-	repo := NewJobRepository()
+func TestSaveTableDriven(t *testing.T) {
 	tests := []struct {
 		name    string
-		status  string
-		jobType string
-		input   any
-		want    any
+		job     *Job
+		wantID  int64
+		wantErr string
 	}{
 		{
-			"Save email job", "queued", "Send Email", EmailInput{
-				Email:   "kaisel@uber.com",
-				Message: "Hola"}, 1,
+			"Save email job", &Job{
+				Status:  "queued",
+				JobType: "Send Email",
+				Input: EmailInput{
+					Email:   "kaisel@uber.com",
+					Message: "Hola",
+				},
+			}, 1, "",
 		},
 		{
-			"Save Meeting job", "queued", "classify meeting", MeetingInput{
-				MeetingID: "123445678",
-				Summary:   "I am a summary from meeting input",
-			}, 2,
+			"Save Meeting job", &Job{
+				Status: "queued", JobType: "classify meeting", Input: MeetingInput{
+					MeetingID: "123445678",
+					Summary:   "I am a summary from meeting input",
+				},
+			}, 1, "",
 		},
 		{
-			"Save null job", "", "nil job", nil, "Job must not be nil",
+			"Save null job", nil, 0, "Job must not be nil",
 		},
 	}
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			job, err := repo.Save(&Job{
-				Status:  tt.status,
-				JobType: tt.jobType,
-				Input:   tt.input,
-			})
-			if err != nil && err != tt.want {
-				t.Errorf("Save(nil job) = %s; want %s", err, tt.want)
+			repo := NewJobRepository()
+			job, err := repo.Save(tt.job)
+			if err != nil && err.Error() != tt.wantErr {
+				t.Errorf("Save(nil job) = %s; want %s", err, tt.wantErr)
 			}
-			if job.ID != tt.want {
-				t.Errorf("Job ID = %d; want %d", job.ID, tt.want)
+			if job.ID != tt.wantID {
+				t.Errorf("Job ID = %d; wantID %d", job.ID, tt.wantID)
 			}
 
 		})
