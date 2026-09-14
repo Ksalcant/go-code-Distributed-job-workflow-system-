3a83c66c0a6689e6e36529c4d2c16a25d8da7e7c
Kaisel Alcantara <Kaise@MacBook-Air.local>
2026-09-14 13:54:25 -0700

    Add first edge cases for the service job creation

--- main.go
+++ main.go
@@ -2,4 +2,16 @@ package main
 
 func main() {
 	// application logic here
+	/*Dependency Injection
+	main()
+	 │
+	 ├─ creates Repository
+	 │
+	 ├─ gives Repository to Service
+	 │
+	 └─ gives Service to Handler/
+	 repo := NewJobRepository()
+	 servie := NewJobService(repo)
+	 handler := NewJobHandler(service)
+	*/
 }
---
+++ service/service_job.go
@@ -0,0 +1,72 @@
+package service
+
+/*
+	Wich operation is valid in our system?
+
+Is this a supported job type?
+What status should a new job have?
+Is the input valid for this operation?
+Is this state transition allowed?
+*/
+import (
+	"errors"
+	"job-system/model"
+	"job-system/repository"
+)
+
+var jobTypes = map[string]struct{}{
+	"send_email":       {},
+	"classify_meeting": {},
+}
+
+type JobService struct {
+	repo repository.JobRepository
+}
+
+func NewJobService(repo repository.JobRepository) *JobService {
+
+	return &JobService{
+		repo: repo,
+	}
+}
+func (srv *JobService) CreateJob(jobType string, jobInput any) (model.Job, error) {
+
+	_, ok := jobTypes[jobType]
+	if !ok {
+		return model.Job{}, errors.New("unsupported job type")
+	}
+
+	if jobInput == nil {
+		return model.Job{}, errors.New("Job input must not be nil")
+	}
+
+	switch jobType {
+	case "send_email":
+		emailInput, ok := jobInput.(model.EmailInput)
+		if !ok {
+			return model.Job{}, errors.New("job input must be EmailInput")
+		}
+		if emailInput.Email == "" {
+			return model.Job{}, errors.New("email must not be empty")
+		}
+		if emailInput.Message == "" {
+			return model.Job{}, errors.New("message must not be empty")
+		}
+	case "classify_meeting":
+		meetingInput, ok := jobInput.(model.MeetingInput)
+		if !ok {
+			return model.Job{}, errors.New("job input must be meetingInput")
+		}
+		if meetingInput.MeetingID == "" {
+			return model.Job{}, errors.New("meetingID must not be empty")
+		}
+		if meetingInput.Summary == "" {
+			return model.Job{}, errors.New("summary must not be empty")
+		}
+
+	default:
+		return model.Job{}, errors.New("unsupported job type")
+	}
+	return model.Job{}, nil
+
+}
